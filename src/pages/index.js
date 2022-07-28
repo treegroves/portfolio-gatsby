537331dc1f9a6c55383869b73602190d89faa618
tim-greenslade <80554755+treegroves@users.noreply.github.com>
2022-07-28 17:42:56 +1200

    Add basic components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import Layout from "../components/Layout"
+import styles from "../styles/home.module.css"
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={styles.header}>
         <div>
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
