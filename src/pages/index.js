2ef97006dede44d330ac8cca2bd07214b1bb0a7f
tim-greenslade <80554755+treegroves@users.noreply.github.com>
2022-07-29 18:44:20 +1200

    Add remplate for generating pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,13 @@ import React from "react"
 import { graphql, Link } from "gatsby"
 import Layout from "../components/Layout"
 import * as styles from "../styles/home.module.css"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 export default function Home({ data }) {
   return (
     <Layout>
       <section className={styles.header}>
-        <img className={styles.avatar} src="/profile.jpg" alt="Profile" />
+        <GatsbyImage image={data.file.childImageSharp.gatsbyImageData} />
         <div>
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
@@ -20,3 +21,13 @@ export default function Home({ data }) {
     </Layout>
   )
 }
+
+export const query = graphql`
+  query Avatar {
+    file(relativePath: { eq: "profile.jpg" }) {
+      childImageSharp {
+        gatsbyImageData
+      }
+    }
+  }
+`
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,6 +2,7 @@ import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../../components/Layout"
 import * as styles from "../../styles/projects.module.css"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 export default function Projects({ data }) {
   console.log(data)
@@ -16,6 +17,12 @@ export default function Projects({ data }) {
           {projects.map(project => (
             <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
               <div>
+                <GatsbyImage
+                  className={styles.avatar}
+                  image={
+                    project.frontmatter.thumb.childImageSharp.gatsbyImageData
+                  }
+                />
                 <h3>{project.frontmatter.title}</h3>
                 <p>{project.frontmatter.stack}</p>
               </div>
@@ -39,6 +46,11 @@ export const query = graphql`
           title
           stack
           slug
+          thumb {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
         }
         id
       }
